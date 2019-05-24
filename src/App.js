aed676ba7f5ba00fae419ca670464eea2598e400
MikeRGarrity <mikergarrity@gmail.com>
2019-05-24 18:25:47 -0500

    added dummy data to title and aggregate counters

--- src/App.js
+++ src/App.js
@@ -5,11 +5,103 @@ let defaultStyle = {
   color: '#fff'
 };
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'Mike',
+    playlists: [
+      {
+        name: 'My favorites',
+        songs: [
+          {
+            name: 'Map of the World',
+            duration: 1234
+          },
+          {
+            name: 'Friends',
+            duration: 1314
+          },
+          {
+            name: 'The Mighty Rio Grande',
+            duration: 1432
+          },
+        ]
+      },
+      {
+        name: 'Discover Weekly',
+        songs: [
+          {
+            name: 'Quiet',
+            duration: 1254
+          },
+          {
+            name: 'A Three-Legged Workhorse',
+            duration: 1514
+          },
+          {
+            name: 'Mizzy C',
+            duration: 1232
+          },
+        ]
+      },
+      {
+        name: 'Another playlist',
+        songs: [
+          {
+            name: 'Supersede',
+            duration: 1634
+          },
+          {
+            name: 'Interloper',
+            duration: 1614
+          },
+          {
+            name: 'Derelects',
+            duration: 1332
+          },
+        ]
+      },
+      {
+        name: 'Workout playlist',
+        songs: [
+          {
+            name: 'Cascade',
+            duration: 1634
+          },
+          {
+            name: 'Syncage',
+            duration: 1614
+          },
+          {
+            name: 'Alligence',
+            duration: 1332
+          },
+        ]
+      }
+    ]
+  }
+};
+
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, [])
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0)
+    return (
+      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+        <h2>{Math.round(totalDuration / 60)} hours</h2>
       </div>
     );
   }
@@ -43,17 +135,34 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {}
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData })
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>Title</h1>
-        <Aggregate />
-        <Aggregate />
-        <Filter />
-        <Playlist />
-        <Playlist />
-        <Playlist />
-        <Playlist />
+        {this.state.serverData.user ?
+          <div>
+            <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>
+              {this.state.serverData.user.name}'s Playlists
+            </h1>
+            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <Filter />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+          </div> : <h1 style={defaultStyle}>Loading...</h1>
+        }
       </div >
     );
   }
