c951bc75a499335e3dd92ee9435dd0486a0e0bd1
unknown <mikergarrity@gmail.com>
2019-06-08 18:48:41 -0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import React, { Component } from 'react';
+import 'reset-css/reset.css';
 import './App.css';
 import queryString from 'query-string';
 
 let defaultStyle = {
-  color: '#fff'
+  color: '#fff',
+  'font-family': 'Sans-serif'
 };
+let counterStyle = {...defaultStyle,
+  display: 'inline-block', 
+  width: '40%',
+  'margin-top': '20px',
+  'margin-bottom': '10px',
+  'font-size': '24px',
+  'line-height': '30px'
+}
 
-let fakeServerData = {
-  user: {
-    name: 'Mike',
-    playlists: [
-      {
-        name: 'Discover Weekly',
-        songs: [
-          {
-            name: 'Quiet',
-            duration: 1254
-          },
-          {
-            name: 'A Three-Legged Workhorse',
-            duration: 1514
-          },
-          {
-            name: 'Mizzy C',
-            duration: 1232
-          },
-        ]
-      },
-    
-    ]
-  }
-};
+function isEven(number) {
+  return number % 2
+}
+
 
 class PlaylistCounter extends Component {
   render() {
+    let playlistCounterStyle = counterStyle
     return (
-      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+      <div style={playlistCounterStyle}>
         <h2>{this.props.playlists.length} playlists</h2>
       </div>
     );
@@ -50,9 +40,15 @@ class HoursCounter extends Component {
     let totalDuration = allSongs.reduce((sum, eachSong) => {
       return sum + eachSong.duration
     }, 0)
+    let totalDurationHours = Math.round(totalDuration/60)
+    let isTooLow = totalDurationHours < 10
+    let hoursCounterStyle = { ...counterStyle, 
+      color: isTooLow ? 'red' : 'white',
+      'font-weight': isTooLow ? 'bold' : 'normal',
+    } 
     return (
-      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
-        <h2>{Math.round(totalDuration / 60)} hours</h2>
+      <div style={hoursCounterStyle}>
+        <h2>{totalDurationHours} hours</h2>
       </div>
     );
   }
@@ -64,7 +60,8 @@ class Filter extends Component {
       <div style={defaultStyle}>
         <img />
         <input type="text" onKeyUp={event => 
-          this.props.onTextChange(event.target.value)}/>
+          this.props.onTextChange(event.target.value)}
+          style={{...defaultStyle, color: 'black', 'font-size': '20px', padding: '10px'}}/>
       </div>
     );
   }
@@ -74,12 +71,19 @@ class Playlist extends Component {
   render() {
     let playlist = this.props.playlist
     return (
-      <div style={{ ...defaultStyle, display: 'inline-block', width: '25%' }}>
+      <div style={{...defaultStyle, 
+        display: 'inline-block', 
+        width: '20%',
+        padding: '10px',
+        'background-color': isEven(this.props.index) 
+          ? '#C0C0C0' 
+          : '#808080'
+       }}>
         <img src={playlist.imageUrl} style={{width: '60px'}}/>
         <h3>{playlist.name}</h3>
-        <ul>
+        <ul style={{'margin-top': '10px'}}>
           {playlist.songs.map(song => 
-            <li>{song.name}</li>
+            <li style={{'padding-top': '2px', 'font-weight': 'bold'}}>{song.name}</li>
           )}
         </ul>
       </div>
@@ -165,7 +169,10 @@ class App extends Component {
       <div className="App">
         {this.state.user ?
           <div>
-            <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>
+            <h1 style={{ ...defaultStyle, 
+              'font-size': '54px', 
+              'margin-top': '5px'
+            }}>
               {this.state.user.name}'s Playlists
             </h1>
             <PlaylistCounter playlists={playlistToRender} />
@@ -173,8 +180,8 @@ class App extends Component {
             <Filter onTextChange={text => {
               this.setState({filterString: text})
             }}/>
-            {playlistToRender.map(playlist => 
-              <Playlist playlist={playlist} />
+            {playlistToRender.map((playlist, i)  => 
+              <Playlist playlist={playlist} index={i} />
             )}
           </div> : <button onClick={() => {
               window.location = window.location.href.includes('localhost') 
